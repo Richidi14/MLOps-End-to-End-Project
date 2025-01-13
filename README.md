1f2bfd65ba2b071c96ed34121183a511be86dc2c
Richidi14 <richardchidiebere@gmail.com>
2025-01-13 02:25:13 +0100

    Folder structure editted

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # MLOps-End-to-End-Project
-Anaconda: https://www.anaconda.com
-VS code: https://code.visualstudio.com/dowload
-Git: https://git-scm-com
+- Anaconda: https://www.anaconda.com
+- VS code: https://code.visualstudio.com/dowload
+- Git: https://git-scm-com
+- Flowchart: https://Whimsical.com/
+- MLOps Tool: https://evidentlyai.com/
--- template.py
+++ template.py
@@ -23,6 +23,14 @@ list_of_files = [
     f"{project_name}/pipeline/prediction_pipeline.py",
     f"{project_name}/utils/__init__.py",
     f"{project_name}/utils/main_utils.py",
+    "app.py",
+    "requirements.txt",
+    "Dockerfile",
+    ".dockerignore",
+    "demo.py",
+    "setup.py",
+    "config/model.yaml",
+    "config/schema.yaml",
 ]
 
 
@@ -31,7 +39,7 @@ for filepath in list_of_files:
     filepath = Path(filepath)
     filedir, filename =os.path.split(filepath)
     if filedir != "":
-        os.makedirs(filedir, exists_ok = True)
+        os.makedirs(filedir, exist_ok = True)
     if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
         with open(filepath, 'w') as f:
             pass
